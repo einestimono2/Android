0ee68f66040ed3a0124e7e91d6a884109afe5b29
Tee <einestimono2@gmail.com>
2022-11-25 21:34:02 +0700

    Rename Project

---
+++ CalculatorUIConstraintLayout/app/src/main/java/com/example/calculatorui_constraintlayout/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.calculatorui_constraintlayout;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.calculatorui_constraintlayout.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
